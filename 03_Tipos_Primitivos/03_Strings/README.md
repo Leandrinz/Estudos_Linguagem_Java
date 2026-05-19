46f8adee682253b3244ebb9f7623bff2c66cd739
Leandrinz <lsavio516@gmail.com>
2026-05-19 09:44:43 -0300

    Strings

---
+++ 03_Tipos_Primitivos/03_Strings/README.md
@@ -0,0 +1,22 @@
+# STRINGS - JAVA
+Strings é um tipo de variável que NÃO é primitivo, que podem armazenar um texto.
+
+## 1) Como declarar uma String:
+Como String é uma classe em Java, ela precisa começar com a letra MAIÚSCULA.
+
+Exemplo:
+```Java
+String nome;
+```
+
+## 2) Como iniciar uma String:
+Exemplo:
+```Java
+String nome = "Leandro";
+```
+
+Printando na tela:
+```Java
+String nome = "Vasco";
+System.out.println("O maior clube do mundo é o "+nome);
+```
--- src/academy/devdojo/maratonajava/Aula02_TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02_TiposPrimitivos.java
@@ -2,14 +2,15 @@ package academy.devdojo.maratonajava;
 
 public class Aula02_TiposPrimitivos {
     public static void main(String[] args) {
+        // ---- AULA 10 ----
         // int, double, float, char, byte, short, long, boolean
         // Exemplo: tipo nomeComposto(primeiro o tipo, depois o nome COMEÇANDO COM MINÚSCULO
         int idade;
         int age = 10; // É assim que iniciamos
-        //System.out.print(age); // É assim que printamos o valor, sem aspas duplas
-        //System.out.print("A idade é "+age+" anos"); // Outra forma de printar (existem outras maneiras, mas vamos ver mais pra frente)
+        System.out.println(age); // É assim que printamos o valor, sem aspas duplas
+        System.out.println("\nA idade é "+age+" anos\n"); // Outra forma de printar (existem outras maneiras, mas vamos ver mais pra frente)
 
-
+        // ---- AULA 11 ----
         double salarioDouble = 2000;
         float salarioFloat = 3000;
         byte idadeByte = 10;
@@ -17,5 +18,9 @@ public class Aula02_TiposPrimitivos {
         boolean verdadeiroBool = true;
         boolean falseBool = false;
         char caractere = 10;
+
+        // ---- AULA 13 ----
+        String nome = "Vasco";
+        System.out.println("O maior clube do mundo é o "+nome);
     }
 }
