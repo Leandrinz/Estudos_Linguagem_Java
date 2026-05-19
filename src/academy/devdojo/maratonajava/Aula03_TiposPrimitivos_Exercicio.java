f74f8eed25daa4f890912c79eceec83b6c32d602
Leandrinz <lsavio516@gmail.com>
2026-05-19 09:59:55 -0300

    Exercício

---
+++ src/academy/devdojo/maratonajava/Aula03_TiposPrimitivos_Exercicio.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava;
+
+public class Aula03_TiposPrimitivos_Exercicio {
+    public static void main() {
+        String nome = "Leandro";
+        String endereco = "Pau dos Ferros RN";
+        int salario = 2583;
+        String data = "19/05/2026";
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data);
+    }
+}
